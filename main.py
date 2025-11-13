4b76bfd3c8965a8bd5daebf0cd1cce45150d48a4
yduan404 <yaqi.duan@student.uva.nl>
2025-11-13 15:35:17 +0100

    part1

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# part 1: calculate arrival rate
+
+airport_data = pd.read_csv('airport.csv', usecols=['Year', 'Month','Europe Passengers', 'Intercontinental Passengers', 'Total Passengers'])
+
+sept_15_19 = airport_data[(airport_data["Year"] >= 2015)
+                    & (airport_data["Year"] <= 2019)
+                    & (airport_data["Month"] == "September")]
+
+hours = 16
+days = 30
+lanes = 50
+
+arrival_rate = sept_15_19['Total Passengers'].sum() / (hours * days * lanes)
+print(arrival_rate)
